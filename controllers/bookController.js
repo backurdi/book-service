8ce68ec6b7ae2991c367a3f4b13f6d99a419ee86
backurdi <bachir.kurdi@gmail.com>
2021-10-19 14:08:57 +0200

    Update bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,5 @@
 const Books = require('../models/bookModel');
 const factory = require('./handlerFactory');
-// test comment
 
 exports.setUserId = (req, res, next) => {
     if (!req.body.user) req.body.user = req.user.id;
