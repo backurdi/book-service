31f619bc796d08ca6bde435a190f535574ac6094
backurdi <bachir.kurdi@gmail.com>
2022-02-04 00:58:40 +0100

    Specify origin

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'pug');
 // app.set('views', path.join(__dirname, 'views' ))
 
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://www.app.reaflect.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested, Content-Type, Accept Authorization');
 
