47c04a8badce36dc6f509f1c5e727ddc4641caee
backurdi <bachir.kurdi@gmail.com>
2022-02-04 18:16:42 +0100

    Update imageController.js

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -7,7 +7,7 @@ exports.setS3Config = (req, res, next)=>{
         setConfigVars('clubPicture');
     }else if(key.includes('profile')){
         setConfigVars('profilePic');
-    }else if(key.includes('post' || 'comment')){
+    }else if(key.includes('post') || key.includes('comment')){
         setConfigVars('postPicture');
     }
     req.s3FileName = 'club';
