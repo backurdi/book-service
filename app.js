15942750d2f1dbe12dcad38e0440e82001b084c6
backurdi <bachir.kurdi@gmail.com>
2022-03-05 09:14:52 +0100

    Fixed cors error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const path = require('path');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const AppError = require('./utils/appError');
@@ -20,7 +20,6 @@ const imageRouter = require('./routes/imageRoutes');
 const clubRouter = require('./routes/clubRoutes');
 const postRouter = require('./routes/postRoutes');
 const notificationRouter = require('./routes/notificationRoutes');
-
 
 const app = express();
 
@@ -42,9 +41,9 @@ app.set('view engine', 'pug');
 //       next();
 //     }
 // };
-const origins = process.env.URL.split(' ')
+const origins = process.env.URL.split(', ');
 app.use(cors({ origin: origins, credentials: true }));
-app.options('*', cors());
+// app.options('*', cors());
 
 // app.use(allowCrossDomain);
 
@@ -52,11 +51,11 @@ app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Set security HTTP headers
-app.use(helmet())
+app.use(helmet());
 
 //Development Logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 //Limit requests from same API
@@ -68,9 +67,11 @@ if (process.env.NODE_ENV === 'development') {
 // app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({
-    limit: '10kb'
-}));
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -80,16 +81,24 @@ app.use(mongoSanitize());
 app.use(xss());
 
 //Prevent parameter pollution
-app.use(hpp({
-    whitelist: ['duration', 'ratingsAverage', 'ratingsQuantity', 'maxGroupSize', 'difficulty', 'price']
-}))
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 //Test middleware
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
-
 
 //Routes
 app.use('/api/v1/users', userRouter);
@@ -102,8 +111,7 @@ app.use('/api/v1/clubs', clubRouter);
 app.use('/api/v1/posts', postRouter);
 
 app.all('*', (req, res, next) => {
-
-    next(new AppError(`Can't find ${req.originalUrl}`, 404))
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
 
 app.use(globalErrorHandler);
